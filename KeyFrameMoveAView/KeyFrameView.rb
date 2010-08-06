c27765b72107e7c0ad6841edf25c17dfbb1fd0b3
Larry Staton Jr <larry@sweetpeasoftware.com>
2010-08-05 21:40:40 -0400

    Fixed kCAAnimationPaced constant. Removed check for heartPath

--- KeyFrameMoveAView/KeyFrameView.rb
+++ KeyFrameMoveAView/KeyFrameView.rb
@@ -8,13 +8,8 @@ class KeyFrameView < NSView
   
   attr_accessor :mover
   
-  def addBounceAnimation
-    animationDict = NSDictionary.dictionaryWithObject(self.originAnimation, forKey:"frameOrigin")
-    @mover.setAnimations(animationDict)
-  end
-  
   def initWithFrame(frame)
-    super.initWithFrame(frame)
+    super
     xInset = 3.0 * (NSWidth(frame)/8.0)
     yInset = 3.0 * (NSHeight(frame)/8.0)
     moverFrame = NSInsetRect(frame, xInset, yInset)
@@ -26,38 +21,43 @@ class KeyFrameView < NSView
     self
   end
   
-  def setFrameOrigin(newOrigin)
-    super.setFrameOrigin(newOrigin)
-  end
-  
-  def heartPath
-    f = @mover.frame
-    if !heartPath
-      heartPath = CGPathCreateMutable()
-      CGPathMoveToPoint(heartPath, NULL, NSMinX(f), NSMinY(f))
-      CGPathAddLineToPoint(heartPath, NULL, NSMinX(f) - NSWidth(f), NSMinY(f) + NSHeight(f) * 0.85)
-      CGPathAddLineToPoint(heartPath, NULL, NSMinX(f), NSMinY(f) - NSHeight(f) * 1.5)
-      CGPathAddLineToPoint(heartPath, NULL, NSMinX(f) + NSWidth(f), NSMinY(f) + NSHeight(f) * 0.85)
-      CGPathAddLineToPoint(heartPath, NULL, NSMinX(f), NSMinY(f))
-      CGPathCloseSubpath(heartPath)
-    end
-    return heartPath
+  def addBounceAnimation
+    animationDict = NSDictionary.dictionaryWithObject(self.originAnimation, forKey:"frameOrigin")
+    @mover.setAnimations(animationDict)
   end
   
   def originAnimation
     originAnimation = CAKeyframeAnimation.animation
     originAnimation.path = self.heartPath
-    originAnimation.duration = 2.0
-    originAnimation.calculationMode = kCAAnimationPaced
+    originAnimation.duration = 3.0
+    originAnimation.calculationMode = "paced"
     return originAnimation
   end
   
+  def heartPath
+    f = @mover.frame
+
+    heartPath = CGPathCreateMutable()
+    CGPathMoveToPoint(heartPath, nil, NSMinX(f), NSMinY(f));
+    CGPathAddLineToPoint(heartPath, nil, NSMinX(f) - NSWidth(f), NSMinY(f) + NSHeight(f) * 0.85);
+    CGPathAddLineToPoint(heartPath, nil, NSMinX(f), NSMinY(f) - NSHeight(f) * 1.5);
+    CGPathAddLineToPoint(heartPath, nil, NSMinX(f) + NSWidth(f), NSMinY(f) + NSHeight(f) * 0.85);
+    CGPathAddLineToPoint(heartPath, nil, NSMinX(f), NSMinY(f));
+    CGPathCloseSubpath(heartPath);
+      
+    return heartPath
+  end
+
   def acceptsFirstResponder
     true
   end
   
   def keyDown(event)
     self.bounce
+  end
+  
+  def setFrameOrigin(newOrigin)
+    super.setFrameOrigin(newOrigin)
   end
   
   def bounce
