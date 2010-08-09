adf9326644c49fd4b9887c33b609807703e4e3ed
Larry Staton Jr <larry@sweetpeasoftware.com>
2010-08-09 11:29:27 -0400

    Default animations set.

--- BackgroundFilteredView/BackgroundFilteredView.rb
+++ BackgroundFilteredView/BackgroundFilteredView.rb
@@ -4,6 +4,8 @@
 # Created by Larry Staton Jr. on 8/8/10.
 # Copyright 2010 Sweetpea Software. All rights reserved.
 
+KCAMediaTimingFunctionEaseInEaseOut = "easeInEaseOut"
+
 class BackgroundFilteredView < NSView
   
   attr_accessor :controls
@@ -24,19 +26,45 @@ class BackgroundFilteredView < NSView
   
   def awakeFromNib
     self.setWantsLayer(true)
-    # self.applyFilter
+    self.applyFilter
   end
   
   def applyFilter
-    
+    center = CIVector.vectorWithX(NSMidX(self.bounds), Y:NSMidY(self.bounds))
+    torus = CIFilter.filterWithName("CITorusLensDistortion")
+    torus.setDefaults
+    torus.name = "torus"
+
+    controls.setBackgroundFilters(NSArray.arrayWithObjects(torus, nil))
+    self.addAnimationToTorusFilter
+  end
+  
+  def addAnimationToTorusFilter
+    keyPath = NSString.stringWithFormat("backgroundFilters.torus.%@", KCIInputWidthKey)
+    animation = CABasicAnimation.animationWithKeyPath(keyPath)
+    animation.fromValue = NSNumber.numberWithFloat(50.0)
+    animation.toValue = NSNumber.numberWithFloat(80.0)
+    animation.duration = 1.0
+    animation.repeatCount = 1e100
+    animation.timingFunction = CAMediaTimingFunction.functionWithName(KCAMediaTimingFunctionEaseInEaseOut)
+    animation.autoreverses = true
+    controls.layer.addAnimation(animation, forKey:"torusAnimation")
   end
   
   def addFilter(sender)
-    
+    if controls.backgroundFilters || (self.backgroundFilters.count >= 0)
+      self.applyFilter
+    end
   end
   
   def removeFilter(sender)
-    
+    if controls.backgroundFilters || (self.backgroundFilters.count >= 0)
+      self.deleteFilter
+    end
+  end
+  
+  def deleteFilter
+    controls.setBackgroundFilters(nil)
   end
   
 end
